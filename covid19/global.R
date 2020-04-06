d541398932576e0e20b853a03c74ad7e418c4e7b
Trey Pujats <tpujats@gmail.com>
2020-04-06 09:25:25 -0400

    commit

--- covid19/global.R
+++ covid19/global.R
@@ -76,19 +76,6 @@ CovidDeaths[is.na(CovidDeaths)]<-0
 colnames(CovidConfirmedCases)[1]<-"CountyFIPS"
 
 
-<<<<<<< HEAD
-# #Read in IHME data for projecting data in the future
-# temp <- tempfile()
-# download.file("https://ihmecovid19storage.blob.core.windows.net/latest/ihme-covid19.zip", temp, mode="wb")
-# zipdf <- unzip(temp, list = TRUE)
-# csv_file <- zipdf$Name[2]
-# IHME_Model <- read.csv(csv_file)
-# unlink(temp)
-# IHME_Model$date <- as.Date(IHME_Model$date, format = "%Y-%m-%d")
-# StateList <- data.frame(state.name, state.abb)
-# IHME_Model <- merge(IHME_Model, StateList, by.x = "location", by.y = names(StateList)[1])
-# names(IHME_Model)[names(IHME_Model)=="state.abb"] <- "State"
-=======
 #Read in IHME data for projecting data in the future
 temp <- tempfile()
 download.file("https://ihmecovid19storage.blob.core.windows.net/latest/ihme-covid19.zip", temp, mode="wb")
@@ -100,7 +87,6 @@ IHME_Model$date <- as.Date(IHME_Model$date, format = "%Y-%m-%d")
 StateList <- data.frame(state.name, state.abb)
 IHME_Model <- merge(IHME_Model, StateList, by.x = "location", by.y = names(StateList)[1])
 names(IHME_Model)[names(IHME_Model)=="state.abb"] <- "State"
->>>>>>> 3726d16ddf174622c917f414944c03a5cc662193
 
 
 #Create list of hospitals, bases, and counties.
