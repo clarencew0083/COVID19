67df106db35749bb56b1abc0966420a7d5d10e78
galarcon0308 <52179539+galarcon0308@users.noreply.github.com>
2020-04-08 10:55:24 -0400

    Added distance to county table and sorted by closest county

--- covid19/global.R
+++ covid19/global.R
@@ -1090,9 +1090,10 @@ GetLocalDataTable<-function(IncludedCounties){
     DeathCounties<-subset(CovidDeaths, CountyFIPS %in% IncludedCounties$FIPS)
     CaseRate <- subset(CovidConfirmedCasesRate, CountyFIPS %in% IncludedCounties$FIPS)
     CountyDataTable<-cbind(IncludedCounties,rev(CovidCounties)[,1],rev(DeathCounties)[,1],rev(CaseRate)[,1])
-    CountyDataTable<-data.frame(CountyDataTable$State,CountyDataTable$County,CountyDataTable$Population, rev(CountyDataTable)[,3], rev(CountyDataTable)[,2],rev(CountyDataTable)[,1])
-    colnames(CountyDataTable)<-c("State","County","Population","Total Confirmed Cases","Total Fatalities", "Case Doubling Rate (days)" )
-    CountyDataTable
+    CountyDataTable<-data.frame(CountyDataTable$State,CountyDataTable$County,CountyDataTable$Population, rev(CountyDataTable)[,3], rev(CountyDataTable)[,2],rev(CountyDataTable)[,1],round(CountyDataTable$DistanceMiles,0))
+    colnames(CountyDataTable)<-c("State","County","Population","Total Confirmed Cases","Total Fatalities", "Case Doubling Rate (days)", "Distance" )
+    CountyDataTable1 <- CountyDataTable[order(CountyDataTable$Distance),]
+    CountyDataTable1
 }
 
 
