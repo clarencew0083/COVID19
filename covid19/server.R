bbc0b000a3172019037db37358a7bb0593782ddc
Auburngrads <Auburngrads@live.com>
2020-04-09 14:17:13 -0400

    reporting seems to work

--- covid19/server.R
+++ covid19/server.R
@@ -376,32 +376,32 @@ server <- function(input, output) {
     
     
 
-    # output$report <- downloadHandler(
-    #     # For PDF output, change this to "report.pdf"
-    #     filename = "report.html",
-    #     content = function(file) {
-    #         # Copy the report file to a temporary directory before processing it, in
-    #         # case we don't have write permissions to the current working dir (which
-    #         # can happen when deployed).
-    #         tempReport <- file.path(tempdir(), "TestReport.Rmd")
-    #         file.copy("TestReport.Rmd", tempReport, overwrite = TRUE)
-    # 
-    #         # Set up parameters to pass to Rmd document
-    #         params <- list(radius = input$Radius,
-    #                        base = input$Base,
-    #                        pjDays = input$proj_days,
-    #                        socDis = input$social_dist)
-    # 
-    #         # Knit the document, passing in the `params` list, and eval it in a
-    #         # child of the global environment (this isolates the code in the document
-    #         # from the code in this app).
-    #         rmarkdown::render(tempReport, output_file = file,
-    #                           params = params,
-    #                           envir = new.env(parent = globalenv())
-    #                           )
-    #         
-    #     }
-    # )
+    output$report <- downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.html",
+        content = function(file) {
+            # Copy the report file to a temporary directory before processing it, in
+            # case we don't have write permissions to the current working dir (which
+            # can happen when deployed).
+            tempReport <- file.path(tempdir(), "TestReport.Rmd")
+            file.copy("TestReport.Rmd", tempReport, overwrite = TRUE)
+
+            # Set up parameters to pass to Rmd document
+            params <- list(radius = input$Radius,
+                           base = input$Base,
+                           pjDays = input$proj_days,
+                           socDis = input$social_dist)
+
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv())
+                              )
+
+        }
+    )
     
     
 }
